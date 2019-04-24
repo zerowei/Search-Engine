d3e6d41e07d55bb3c4525ecfdf993df30024ab2a
Rui Guo <ruig2@uci.edu>
2019-04-23 22:14:08 -0700

    fix typo

--- src/test/java/edu/uci/ics/cs221/index/Team11MergeSearchTest.java
+++ src/test/java/edu/uci/ics/cs221/index/Team11MergeSearchTest.java
@@ -44,12 +44,12 @@ public class Team11MergeSearchTest {
 
     @Test
     public void mergeSearchTest1() {
-        InvertedIndexManager.DEFAULT_FLUSH_THRESHOLD = 5;
+        InvertedIndexManager.DEFAULT_MERGE_THRESHOLD = 5;
 
         for (Document doc : documents) {
             index.addDocument(doc);
             index.mergeAllSegments();
-            assert index.getNumSegments() < InvertedIndexManager.DEFAULT_FLUSH_THRESHOLD;
+            assert index.getNumSegments() < InvertedIndexManager.DEFAULT_MERGE_THRESHOLD;
         }
     }
 
