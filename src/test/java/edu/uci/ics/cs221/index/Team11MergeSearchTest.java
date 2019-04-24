d51acef262f87b382b463acc5ebaee23b9862656
Rui Guo <ruig2@uci.edu>
2019-04-23 22:04:22 -0700

    added clean()

--- src/test/java/edu/uci/ics/cs221/index/Team11MergeSearchTest.java
+++ src/test/java/edu/uci/ics/cs221/index/Team11MergeSearchTest.java
@@ -6,33 +6,51 @@ import edu.uci.ics.cs221.analysis.PorterStemmer;
 import edu.uci.ics.cs221.analysis.PunctuationTokenizer;
 import edu.uci.ics.cs221.index.inverted.InvertedIndexManager;
 import edu.uci.ics.cs221.storage.Document;
+import org.junit.After;
 import org.junit.Test;
 
+import java.io.File;
 
 public class Team11MergeSearchTest {
+    String indexPath = "index_path";
 
     // Back to December by Taylor Swift
     Document[] documents = new Document[] {
         new Document("This morning I ate eggs"),
-            new Document("I'm so glad you made time to see me"),
-            new Document("How's life, tell me how's your family?"),
-            new Document("I haven't seen them in a while"),
-            new Document("You've been good, busier then ever"),
-            new Document("We small talk, work and the weather"),
-            new Document("Your guard is up and I know why")
+        new Document("I'm so glad you made time to see me"),
+        new Document("How's life, tell me how's your family?"),
+        new Document("I haven't seen them in a while"),
+        new Document("You've been good, busier then ever"),
+        new Document("We small talk, work and the weather"),
+        new Document("Your guard is up and I know why")
     };
 
     Analyzer analyzer = new ComposableAnalyzer(new PunctuationTokenizer(), new PorterStemmer());
-    InvertedIndexManager index = InvertedIndexManager.createOrOpen("index_path", analyzer);
+    InvertedIndexManager index = InvertedIndexManager.createOrOpen(indexPath, analyzer);
+
+    @After
+    public void clean() {
+        try {
+            File file = new File(indexPath);
+
+            if (file.delete()) {
+                System.out.println("File deleted successfully");
+            } else {
+                System.out.println("Failed to delete the file");
+            }
+        } catch (Exception e) {
+            System.out.println("Something went wrong when deleting file");
+        }
+    }
 
     @Test
     public void mergeSearchTest1() {
-        index.DEFAULT_FLUSH_THRESHOLD = 5;
+        InvertedIndexManager.DEFAULT_MERGE_THRESHOLD = 5;
 
         for (Document doc : documents) {
             index.addDocument(doc);
-            assert index.getNumSegments() < index.DEFAULT_FLUSH_THRESHOLD;
+            index.mergeAllSegments();
+            assert index.getNumSegments() < InvertedIndexManager.DEFAULT_FLUSH_THRESHOLD;
         }
-
     }
 }
