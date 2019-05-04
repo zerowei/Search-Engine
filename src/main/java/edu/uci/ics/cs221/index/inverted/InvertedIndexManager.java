15a78271812a5b56dd7699c582727e4d350e65a1
Rui Guo <ruig2@uci.edu>
2019-05-03 22:06:00 -0700

    Revert "polishing code"
    This reverts commit d4d5927f2a355e86c8bd67d94448e4097635fee8.

--- src/main/java/edu/uci/ics/cs221/index/inverted/InvertedIndexManager.java
+++ src/main/java/edu/uci/ics/cs221/index/inverted/InvertedIndexManager.java
@@ -108,35 +108,23 @@ public class InvertedIndexManager {
         return;
     }
 
-    private String getHeaderFilePath() {
-        return indexFolder + "/header" + numOfSeg.toString() + ".txt";
-    }
-
-    private String getSegmentFilePath() {
-        return indexFolder + "/segment" + numOfSeg.toString() + ".txt";
-    }
-
     /**
      * Flushes all the documents in the in-memory segment buffer to disk. If the buffer is empty, it should not do anything.
      * flush() writes the segment to disk containing the posting list and the corresponding document store.
      */
     public void flush() {
-
         if (buffer.isEmpty()) {
             return;
         }
-
         Iterator<Map.Entry<Integer, Document>> itr = documents.entrySet().iterator();
         DocumentStore documentStore = MapdbDocStore.createWithBulkLoad(docStorePath, itr);
         documentStore.close();
-
-        String headerFilePath = getHeaderFilePath();
-        String segmentFilePath = getSegmentFilePath();
-
+        String path = indexFolder + "/header" + numOfSeg.toString() + ".txt";
+        String path1 = indexFolder + "/segment" + numOfSeg.toString() + ".txt";
         int len = 0;
         int pageId, offset;
         numOfSeg += 1;
-        Path filePath = Paths.get(headerFilePath);
+        Path filePath = Paths.get(path);
         PageFileChannel pageFileChannel = PageFileChannel.createOrOpen(filePath);
         for (String obj : buffer.keySet()) {
             len = len + obj.getBytes().length + 4 * 4;
@@ -161,7 +149,7 @@ public class InvertedIndexManager {
         buf.clear();
         pageFileChannel.close();
 
-        Path filePath1 = Paths.get(segmentFilePath);
+        Path filePath1 = Paths.get(path1);
         PageFileChannel pageFileChannel1 = PageFileChannel.createOrOpen(filePath1);
         int numOfInts = 0;
         for (List<Integer> appears : buffer.values()) {
@@ -181,7 +169,6 @@ public class InvertedIndexManager {
         if (numOfSeg == InvertedIndexManager.DEFAULT_MERGE_THRESHOLD) {
             mergeAllSegments();
         }
-
         numStores += 1;
         record = 0;
         buffer.clear();
