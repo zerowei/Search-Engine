eea9cea7ca63e32f0ebaba3555e9c78fe05e3ed8
TianyiWei <weit6@uci.edu>
2019-05-15 20:32:21 -0700

    merged and updated for phrase search

--- src/main/java/edu/uci/ics/cs221/index/inverted/InvertedIndex.java
+++ src/main/java/edu/uci/ics/cs221/index/inverted/InvertedIndex.java
@@ -1,28 +1,29 @@
 package edu.uci.ics.cs221.index.inverted;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class InvertedIndex {
     String keyword;
-    List<Integer> documentIds;
-    List<List<Integer>> positions;
+    Map<Integer, List<Integer>> docPositions;
 
-    InvertedIndex(String keyword, List<Integer> documentIds, List<List<Integer>> positions) {
+    InvertedIndex(String keyword, Map<Integer, List<Integer>> docPositions) {
         this.keyword = keyword;
-        this.documentIds = documentIds;
-        this.positions = positions;
+        this.docPositions = docPositions;
     }
 
     @Override public String toString() {
-        assert positions.size() == documentIds.size();
-        for (int i = 0; i < positions.size(); i++) {
-            assert positions.get(i).size() > 0;
+        System.out.println(docPositions);
+        Iterator<List<Integer>> itr = docPositions.values().iterator();
+        while (itr.hasNext()){
+            assert itr.next().size() > 0;
         }
 
         return "InvertedIndex{" +
                 "keyword: " + keyword +
-                ", documentIds: " + documentIds +
-                " positions: " + positions +
+                ", documentIds: " + docPositions.keySet() +
+                " positions: " + docPositions.values() +
                 "}";
     }
 }
